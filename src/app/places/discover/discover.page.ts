b8583d77b436d251d8666f7241fe7de28151928f
delgado.trueba <delgado.trueba@outlook.es>
2019-07-04 17:02:44 +0200

    SideDrawer

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -12,11 +13,16 @@ export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
 
   constructor(
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
   ) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.getPlaces();
+  }
+
+  onOpenMenu(){
+    this.menuCtrl.toggle("m1");
   }
 
 }
