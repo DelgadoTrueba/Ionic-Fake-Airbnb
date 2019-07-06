bc407592afb4bbcae6a9ee4967c7d201823d5c84
delgado.trueba <delgado.trueba@outlook.es>
2019-07-06 19:34:16 +0200

    MODAL

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -16,7 +17,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
   ) { }
 
   ngOnInit() {
@@ -33,7 +35,26 @@ export class PlaceDetailPage implements OnInit {
 
   onBookPlace(){
     //this.router.navigateByUrl("/places/tabs/discover")
-    this.navCtrl.navigateBack("/places/tabs/discover");
+    //this.navCtrl.navigateBack("/places/tabs/discover");
+
+    this.modalCtrl
+      .create({
+        component: CreateBookingComponent, 
+        componentProps: {selectedPlace: this.place}
+        //id: 
+      })
+      .then( modalEl => {
+        modalEl.present();
+        //return a promise
+        return modalEl.onDidDismiss();
+      })
+      .then( resultData => {
+        console.log(resultData.data, resultData.role);
+        if(resultData.role === "confirm" ){
+          console.log("BOOKED!")
+        }
+      });
+
   }
 
 }
